1534adfddc3e195dd0e55322a8fecd3ecf6ade22
Simounne Cruz <simounnegcruz@iskolarngbayan.pup.edu.ph>
2025-03-24 04:19:47 +0800

    link features

--- pupt-login-system/resources/views/welcome.blade.php
+++ pupt-login-system/resources/views/welcome.blade.php
@@ -42,6 +42,7 @@
         <ul>
           <li><a href="#hero" class="active">Home</a></li>
           <li><a href="#about">About</a></li>
+          <li><a href="#features">Features</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
