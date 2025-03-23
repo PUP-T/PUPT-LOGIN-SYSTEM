522867f47d5d8365f19a2a90e13c08187ec6887d
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2025-03-23 19:37:08 +0800

    add vendor and main js files

--- pupt-login-system/resources/views/welcome.blade.php
+++ pupt-login-system/resources/views/welcome.blade.php
@@ -31,4 +31,17 @@
 <body class="index-page">
 </body>
 
+    <!-- Vendor JS Files -->
+    <script src="assets/landing-page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/landing-page/vendor/php-email-form/validate.js"></script>
+    <script src="assets/landing-page/vendor/aos/aos.js"></script>
+    <script src="assets/landing-page/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/landing-page/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/landing-page/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/landing-page/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/landing-page/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+    <!-- Main JS File -->
+    <script src="assets/landing-page/js/main.js"></script>
+
 </html>
