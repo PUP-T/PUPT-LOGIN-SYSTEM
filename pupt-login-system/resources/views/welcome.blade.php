225ec2bff612a731eea9a345d9a594c77f629ebe
Simounne Cruz <simounnegcruz@iskolarngbayan.pup.edu.ph>
2025-03-24 04:15:03 +0800

    link home

--- pupt-login-system/resources/views/welcome.blade.php
+++ pupt-login-system/resources/views/welcome.blade.php
@@ -40,6 +40,7 @@
 
       <nav id="navmenu" class="navmenu">
         <ul>
+          <li><a href="#hero" class="active">Home</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
