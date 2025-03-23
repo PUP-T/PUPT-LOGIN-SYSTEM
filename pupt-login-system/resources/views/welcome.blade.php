058933cd1313537e4ac708de557aa02edb70c7a0
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2025-03-23 19:30:51 +0800

    add meta tags and title

--- pupt-login-system/resources/views/welcome.blade.php
+++ pupt-login-system/resources/views/welcome.blade.php
@@ -2,6 +2,11 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>PUP-Taguig Systems Authentication</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 </head>
 
 <body class="index-page">
