43ce39a5a4e3fde96ae83ab6fd0b0e310ff2adbb
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-03-23 22:52:56 +0800

    add pup logo

--- pupt-login-system/resources/views/login.blade.php
+++ pupt-login-system/resources/views/login.blade.php
@@ -7,5 +7,14 @@
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
+    <div class="main-container">
+        <div class="bg-image"></div>
+        <div class="diagonal-overlay"></div>
+        <div class="logo-form-bg"></div>
+
+        <div class="logo-container">
+            <img src="assets/images/PUPLogo.png" alt="PUP Logo" class="logo">
+        </div>
+    </div>
 </body>
 </html>
