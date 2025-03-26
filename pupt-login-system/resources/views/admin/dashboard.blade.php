c4c71be54472582761a523d31c994cf14178a0a7
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-03-26 21:48:45 +0800

    Implemented dashboard layout with faculty and student user stats

--- pupt-login-system/resources/views/admin/dashboard.blade.php
+++ pupt-login-system/resources/views/admin/dashboard.blade.php
@@ -160,3 +160,81 @@
           </div>
         </header>
         <!-- ========== header end ========== -->
+
+         <!-- ========== section start ========== -->
+      <section class="section">
+        <div class="container-fluid">
+          <!-- ========== title-wrapper start ========== -->
+          <div class="title-wrapper pt-30">
+            <div class="row align-items-center">
+              <div class="col-md-6">
+                <div class="title">
+                  <h3> Dashboard</h3>
+                </div>
+              </div>
+              <!-- end col -->
+              <div class="col-md-6">
+                <div class="breadcrumb-wrapper">
+
+                </div>
+              </div>
+              <!-- end col -->
+            </div>
+            <!-- end row -->
+          </div>
+          <!-- ========== title-wrapper end ========== -->
+          <div class="row">
+            <div class="col-xl-6 col-lg-4 col-sm-6">
+              <div class="icon-card mb-30">
+                <div class="icon purple">
+                    <i class="lni lni-users"></i>
+
+                </div>
+                <div class="content">
+                  <h6 class="mb-10">Total of Faculty Users</h6>
+                  <h3 class="text-bold mb-10">34567</h3>
+                </div>
+              </div>
+              <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+            <div class="col-xl-6 col-lg-4 col-sm-6">
+              <div class="icon-card mb-30">
+                <div class="icon success">
+                    <i class="lni lni-user"></i>
+                </div>
+                <div class="content">
+                  <h6 class="mb-10">Total of Student Users</h6>
+                  <h3 class="text-bold mb-10">567</h3>
+                </div>
+              </div>
+              <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+
+            <!-- End Col -->
+          </div>
+          <!-- End Row -->
+        </div>
+        <!-- end container -->
+      </section>
+      <!-- ========== section end ========== -->
+      <!-- ========== footer end =========== -->
+    </main>
+    <!-- ======== main-wrapper end =========== -->
+
+    <!-- ========= All Javascript files linkup ======== -->
+    <script src="../../assets/admin/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/admin/js/Chart.min.js"></script>
+    <script src="../../assets/admin/js/dynamic-pie-chart.js"></script>
+    <script src="../../assets/admin/js/moment.min.js"></script>
+    <script src="../../assets/admin/js/fullcalendar.js"></script>
+    <script src="../../assets/admin/js/jvectormap.min.js"></script>
+    <script src="../../assets/admin/js/world-merc.js"></script>
+    <script src="../../assets/admin/js/polyfill.js"></script>
+    <script src="../../assets/admin/js/main.js"></script>
+
+
+  </body>
+</html>
+
