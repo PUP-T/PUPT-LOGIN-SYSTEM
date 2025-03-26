c4c4eb731684d38e778ee2669d3deca038f00b42
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-03-26 22:14:02 +0800

    Added login page structure with CSS and background styling

---
+++ pupt-login-system/resources/views/emails/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PUP-Taguig Systems Authentication</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/PUPLogo.png') }}">
+    <style>
+    .logo-form-bg::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url('assets/images/login.jpg');
+        background-size: cover;
+        background-position: center;
+        border-radius: 20px;
+        z-index: 2;
+    }
+    </style>
+</head>
+
