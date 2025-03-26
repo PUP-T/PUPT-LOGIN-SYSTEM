873cfc9004f83cdfef5d3c57294d9a41297c4c54
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-03-26 21:38:19 +0800

    added sidebar navigation with icons and links for admin dashboard

--- pupt-login-system/resources/views/admin/dashboard.blade.php
+++ pupt-login-system/resources/views/admin/dashboard.blade.php
@@ -17,3 +17,78 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
   </head>
+  <body>
+    <!-- ======== Preloader =========== -->
+    <div id="preloader">
+      <div class="spinner"></div>
+    </div>
+    <!-- ======== Preloader =========== -->
+
+    <!-- ======== sidebar-nav start =========== -->
+    <aside class="sidebar-nav-wrapper">
+      <div class="navbar-logo">
+        <div class="d-flex align-items-center">
+            <a href="index.html">
+              <img src="../../assets/images/PUPLogo.png" alt="logo" style="width: 50px; height: auto;"/>
+            </a>
+            <h6 class="ms-2 fw-bold mb-0" style="color:#7e0e09;">PUP-T Systems Authentication</h6>
+          </div>
+
+      </div>
+      <nav class="sidebar-nav">
+        <ul>
+            <li class="nav-item {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+                <a href="{{ route('admin.dashboard') }}">
+                    <span class="icon">
+                        <i class="fas fa-home"></i>
+                    </span>
+                    <span class="text">Home</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="" >
+                  <span class="icon">
+                    <i class="fas fa-history"></i>
+                  </span>
+                  <span class="text">Audit Trail</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" >
+                  <span class="icon">
+                    <i class="fas fa-users"></i>
+                  </span>
+                  <span class="text">user Management</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" >
+                  <span class="icon">
+                    <i class="fas fa-user-shield"></i>
+                  </span>
+                  <span class="text">Access Control</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" >
+                  <span class="icon">
+                    <i class="fas fa-cog"></i>
+                  </span>
+                  <span class="text">Settings</span>
+                </a>
+            </li>
+            <span class="divider"><hr /></span>
+            <li class="nav-item">
+                <a href="" >
+                  <span class="icon">
+                    <i class="fas fa-sign-out-alt"></i>
+                  </span>
+                  <span class="text">Sign Out</span>
+                </a>
+            </li>
+        </ul>
+      </nav>
+    </aside>
+    <div class="overlay"></div>
+    <!-- ======== sidebar-nav end =========== -->
