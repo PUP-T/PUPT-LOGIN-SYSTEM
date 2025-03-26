0779a42ccc220f178282891b6e5fe9b65dc0a91a
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-03-26 22:16:41 +0800

    Added login page layout with authentication form and styling

--- pupt-login-system/resources/views/emails/login.blade.php
+++ pupt-login-system/resources/views/emails/login.blade.php
@@ -22,4 +22,41 @@
     }
     </style>
 </head>
+<body>
+    <div class="main-container">
+        <div class="bg-image"></div>
+        <div class="diagonal-overlay"></div>
+        <div class="logo-form-bg"></div>
 
+        <div class="logo-container">
+            <img src="assets/images/PUPLogo.png" alt="PUP Logo" class="logo">
+        </div>
+
+        <div class="login-form-container">
+            <div class="login-form">
+                <div class="form-title">
+                    <h2>PUP-Taguig Systems</h2>
+                    <h2>Authentication</h2>
+                </div>
+
+                <form>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" class="form-control" placeholder="Enter your email address">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" class="form-control" placeholder="Enter your password">
+                    </div>
+
+                    <button type="submit" class="sign-in-btn">Sign in</button>
+                    <div class="signup-text">
+                        Don't have an account? <a href="{{{route ('sign-up')}}}" class="signup-link">Sign up</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
