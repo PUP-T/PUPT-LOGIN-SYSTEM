131a4ab49154016b9c7c2db47518531177616de5
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-03-26 21:34:56 +0800

    Added linked essential CSS files

---
+++ pupt-login-system/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="../../assets/images/PUPLogo.png" type="image/x-icon" />
+    <title>Dashboard</title>
+
+    <!-- ========== All CSS files linkup ========= -->
+    <link rel="stylesheet" href="../../assets/admin/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../assets/admin/css/lineicons.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="../../assets/admin/css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="../../assets/admin/css/fullcalendar.css" />
+    <link rel="stylesheet" href="../../assets/admin/css/fullcalendar.css" />
+    <link rel="stylesheet" href="../../assets/admin/css/main.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+  </head>
