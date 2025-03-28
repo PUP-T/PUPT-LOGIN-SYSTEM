d9f0ace584563fc5993d05aa8110d3a42e72d438
mingoedjudah <edjudah.ej@gmail.com>
2025-03-28 17:31:32 +0800

    remove the parent folder and fixed some directories and missing components

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,9 +1,9 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
- 
+
 class AdminController extends Controller
 {
     // Admin dashboard page
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,7 +33,7 @@
             </a>
             <h6 class="ms-2 fw-bold mb-0" style="color:#7e0e09;">PUP-T Systems Authentication</h6>
           </div>
-
+          
       </div>
       <nav class="sidebar-nav">
         <ul>
@@ -45,7 +45,7 @@
                     <span class="text">Home</span>
                 </a>
             </li>
-
+            
             <li class="nav-item">
                 <a href="" >
                   <span class="icon">
@@ -95,73 +95,73 @@
 
     <!-- ======== main-wrapper start =========== -->
     <main class="main-wrapper">
-        <!-- ========== header start ========== -->
-        <header class="header">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-lg-5 col-md-5 col-6">
-                <div class="header-left d-flex align-items-center">
-                  <div class="menu-toggle-btn mr-15">
-                    <button id="menu-toggle" class="main-btn primary-btn btn-hover">
-                      <i class="lni lni-chevron-left me-2"></i> Menu
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-7 col-md-7 col-6">
-                <div class="header-right">
-                  <!-- profile start -->
-                  <div class="profile-box ml-15">
-                    <button class="dropdown-toggle bg-transparent border-0" type="button" id="profile"
-                      data-bs-toggle="dropdown" aria-expanded="false">
-                      <div class="profile-info">
-                        <div class="info">
-                          <div class="image">
-                            <img src="../../assets/admin/images/profile-picture.png" alt="" />
-                          </div>
-                          <div>
-                            <h6 class="fw-500">Ed Mingo</h6>
-                            <p>Admin</p>
-                          </div>
-                        </div>
-                      </div>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profile">
-                      <li>
-                        <div class="author-info flex items-center !p-1">
-                          <div class="image">
-                            <img src="../../assets/admin/images/profile-picture.png" alt="image">
-                          </div>
-                          <div class="content">
-                            <h4 class="text-sm">Ed Mingo</h4>
-                            <a class="text-black/40 dark:text-white/40 hover:text-black dark:hover:text-white text-xs" href="#">Email@gmail.com</a>
-                          </div>
-                        </div>
-                      </li>
-                      <li class="divider"></li>
-                      <li>
-                        <a href="#0">
-                          <i class="lni lni-user"></i> View Profile
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#0"> <i class="lni lni-cog"></i> Settings </a>
-                      </li>
-                      <li class="divider"></li>
-                      <li>
-                        <a href="#0"> <i class="lni lni-exit"></i> Sign Out </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <!-- profile end -->
+      <!-- ========== header start ========== -->
+      <header class="header">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-5 col-md-5 col-6">
+              <div class="header-left d-flex align-items-center">
+                <div class="menu-toggle-btn mr-15">
+                  <button id="menu-toggle" class="main-btn primary-btn btn-hover">
+                    <i class="lni lni-chevron-left me-2"></i> Menu
+                  </button>
                 </div>
               </div>
             </div>
+            <div class="col-lg-7 col-md-7 col-6">
+              <div class="header-right">
+                <!-- profile start -->
+                <div class="profile-box ml-15">
+                  <button class="dropdown-toggle bg-transparent border-0" type="button" id="profile"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="profile-info">
+                      <div class="info">
+                        <div class="image">
+                          <img src="../../assets/admin/images/profile-picture.png" alt="" />
+                        </div>
+                        <div>
+                          <h6 class="fw-500">Ed Mingo</h6>
+                          <p>Admin</p>
+                        </div>
+                      </div>
+                    </div>
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profile">
+                    <li>
+                      <div class="author-info flex items-center !p-1">
+                        <div class="image">
+                          <img src="../../assets/admin/images/profile-picture.png" alt="image">
+                        </div>
+                        <div class="content">
+                          <h4 class="text-sm">Ed Mingo</h4>
+                          <a class="text-black/40 dark:text-white/40 hover:text-black dark:hover:text-white text-xs" href="#">Email@gmail.com</a>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                      <a href="#0">
+                        <i class="lni lni-user"></i> View Profile
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#0"> <i class="lni lni-cog"></i> Settings </a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                      <a href="#0"> <i class="lni lni-exit"></i> Sign Out </a>
+                    </li>
+                  </ul>
+                </div>
+                <!-- profile end -->
+              </div>
+            </div>
           </div>
-        </header>
-        <!-- ========== header end ========== -->
+        </div>
+      </header>
+      <!-- ========== header end ========== -->
 
-         <!-- ========== section start ========== -->
+      <!-- ========== section start ========== -->
       <section class="section">
         <div class="container-fluid">
           <!-- ========== title-wrapper start ========== -->
@@ -175,7 +175,7 @@
               <!-- end col -->
               <div class="col-md-6">
                 <div class="breadcrumb-wrapper">
-
+                 
                 </div>
               </div>
               <!-- end col -->
@@ -211,7 +211,7 @@
               <!-- End Icon Cart -->
             </div>
             <!-- End Col -->
-
+         
             <!-- End Col -->
           </div>
           <!-- End Row -->
@@ -234,7 +234,6 @@
     <script src="../../assets/admin/js/polyfill.js"></script>
     <script src="../../assets/admin/js/main.js"></script>
 
-
+    
   </body>
 </html>
-
